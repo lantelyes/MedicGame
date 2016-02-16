7390d3e0d82f6a7c4a4bf9ed1fe0f8f620118228
Leo Antelyes <antell@rpi.edu>
2016-02-16 16:54:36 -0500

    Names

---
+++ MedicGame_proj/Assets/Scripts/NameReader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NameReader : MonoBehaviour {
+
+    public TextAsset nameList;
+
+    TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+
+        textMesh = GetComponent<TextMesh>();
+
+        textMesh.text = nameList.text;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position -= new Vector3(0.0f, 0.0f, 10.0f) * Time.deltaTime;
+	
+	}
+}
